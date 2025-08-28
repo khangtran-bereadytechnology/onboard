a0537428c21babb45cabfdc531918e9a9ec64b58
khangtran-bereadytechnology <khang.tran@bereadytechnology.com.au>
2025-08-28 12:42:32 +0700

    COIN-454 Nâng cấp Backend cho app TodoList

---
+++ COIN-454/resources/views/layouts/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <title>ToDo App</title>
+</head>
+
+<body class="antialiased">
+    {{-- @include('layouts.header') --}}
+
+    <div>
+        @yield('content')
+    </div>
+</body>
+
+</html>
